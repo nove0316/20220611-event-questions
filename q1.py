0ee91b4b92817a747e138fa7949b332f2e70fdc8
open_egg <56103998+nove0316@users.noreply.github.com>
2022-05-28 22:31:22 +0900

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,9 +1,20 @@
 for num in range(1, 101):
     string = ''
+    
+    # 3の倍数　かつ　5の倍数　である時
+    if num % 3 == 0 and num % 5 == 0:
+        string = 'FizzBuzz'
 
-    # ここから記述
+    # 3の倍数　かつ　5の倍数でない時
+    elif num % 3 == 0:
+        string = 'Fizz'
 
+    # 3の倍数でない　かつ 5の倍数 の時
+    elif num % 5 == 0:
+        string = 'Buzz'
 
-    # ここまで記述
+    # 3の倍数でも5の倍数でもない時
+    else:
+        string = num
 
     print(string)
